2940ef17c69dd1bdf9625da22c7a88bf770658b4
bygeek <byyangatchina@gmail.com>
2019-05-30 17:41:25 +0800

    add socket sample code

---
+++ ch16/sock_client.cpp
@@ -0,0 +1,69 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 10086
+
+void connect_retry(int fd, struct sockaddr* addr, socklen_t len){
+    int err;
+    int MAX_SLEEP = 128;
+    int nsleep =1;
+
+    for(; nsleep < MAX_SLEEP; nsleep = nsleep<<1){
+        err = connect(fd, addr, len);
+        if(err == 0){
+            return;
+        }else{
+            printf("can not connect to server\n");
+        }
+    }
+
+    if(err == 0){
+        return;
+    }else{
+        printf("can not connect to server!");
+        exit(1);
+    }
+}
+
+int main(){
+    //socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1){
+        printf("new socket failed\n");
+        exit(1);
+    }
+
+    //connect
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &address.sin_addr.s_addr);
+
+    connect_retry(sockfd, (sockaddr*)&address, sizeof(sockaddr_in));
+
+
+    //send/recv
+    char buf[] = "hello,world";
+
+    //todo: test 1025 byte?
+    int nsend = send(sockfd, buf, sizeof(buf), 0);
+    printf("send %d bytes\n", nsend);
+
+    shutdown(sockfd, SHUT_WR);
+
+    char recvbuf[1024];
+    int nrecv;
+    while ((nrecv = recv(sockfd, recvbuf, sizeof(recvbuf), 0)) > 0)
+    {
+        buf[nrecv] =0;
+        printf("client recv: %s\n", buf);
+    }
+
+    shutdown(sockfd, SHUT_RD);
+    close(sockfd);
+
+    exit(0);
+}
--- ch16/sock_server.cpp
+++ ch16/sock_server.cpp
@@ -1,1 +1,70 @@
-//#include <
+#include <unistd.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define PORT 10086
+#define BACK_LOG 128
+
+int main(){
+    //socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1){
+        printf("new socket failed\n");
+        exit(1);
+    }
+
+    //bind
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", (void*)&address.sin_addr.s_addr);
+
+    int err = bind(sockfd, (const sockaddr*)&address, sizeof(address));
+    if(err != 0){
+        printf("bind failed\n");
+        close(sockfd);
+        exit(1);
+    }
+
+
+    //listen
+
+    err = listen(sockfd, 128);
+    if(err != 0){
+        printf("listen failed\n");
+        close(sockfd);
+        exit(1);
+    }
+
+    //accept
+    printf("wait for connection...\n");
+    int clientfd = accept(sockfd, NULL, NULL);
+    printf("got a connection!\n");
+
+
+    //fork another process to handle new connection?
+
+    char buf[1024];
+    int nrecv;
+    int ntotol_recv =0;
+    while((nrecv =recv(clientfd, buf, sizeof(buf) -1, 0)) >0){
+        buf[nrecv] = 0;
+        printf("server recved: %s\n", buf);
+        ntotol_recv += nrecv;
+    }
+
+    printf("server prepare to send\n");
+
+    snprintf(buf, sizeof(buf), "got %d bytes", ntotol_recv);
+
+    send(clientfd, buf, sizeof(buf), 0);
+
+    shutdown(clientfd, SHUT_RDWR);
+    close(clientfd);
+
+    close(sockfd);
+
+}
